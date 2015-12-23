61d025435a786467c744862bd8cb247e4fa6afbd
yuta yokoi <dezirus.novajn.iojn@gmail.com>
2015-12-23 19:38:44 +0900

    create_news_items add

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 
--- application/models/News_model.php
+++ application/models/News_model.php
@@ -16,4 +16,17 @@ class News_model extends CI_Model
         $query = $this->db->get_where('news', array('slug' => $slug));
         return $query->row_array();
     }
+
+    public function set_news()
+    {
+        $this->load->helper('url');
+
+        $slug = url_title($this->input->post('title'), 'dash', true);
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'text' => $this->input->post('text')
+        );
+        return $this->db->insert('news', $data);
+    }
 }
